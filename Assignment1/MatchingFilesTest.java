022a2683f8cf4ee516c053a7eee209f5445accdd
fareedsheik <59389608+fareedsheik@users.noreply.github.com>
2020-02-27 15:09:19 +0530

    Update MatchingFilesTest.java

--- Assignment1/MatchingFilesTest.java
+++ Assignment1/MatchingFilesTest.java
@@ -5,18 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatchingFilesTest {
-    MatchingFiles matchingFIles = new MatchingFiles();
+    MatchingFiles matchingFiles = new MatchingFiles();
 
     //the method returns the concatinated absolute paths of all files matching the given regular expression otherwise returns Not found
 
     @Test
     void TestCase1() {
         assertEquals("/home/fareed/coding1/home/fareed/coding3/home/fareed/coding2/home/fareed/coding5/home/fareed/coding4"
-                , matchingFIles.searchForMatchedFiles(System.getProperty("user.home"), "[a-z]*[1-5]"));
+                ,matchingFiles.searchForMatchedFiles(System.getProperty("user.home"), "[a-z]*[1-5]"));
     }
     @Test
     void TestCase2() {
-        assertEquals("Not found"
-                , matchingFIles.searchForMatchedFiles(System.getProperty("user.home"), "indian*.java"));
+        assertEquals("Not found",matchingFiles.searchForMatchedFiles(System.getProperty("user.home"), "indian*.java"));
     }
 }
